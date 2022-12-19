8fb28e19af9c31d8a05ce99e1fdaa17367b03f9e
kyle_coder <geeetech.inc@gmail.com>
2022-12-19 11:59:15 +0530

    ''

--- model/prediction.py
+++ model/prediction.py
@@ -3,10 +3,14 @@ import pickle
 
 #doc_new = ['obama is running for president in 2016']
 
+def get_full_path_path(abs_path) :
+    import os.path
+    return os.path.join(os.path.dirname(__file__),abs_path)
+
 #function to run for prediction
 def detect_fake_news(var):    
     #retrieving the best model for prediction call
-    load_model = pickle.load(open('final_model.sav', 'rb'))
+    load_model = pickle.load(open(get_full_path_path('final_model.sav'), 'rb'))
     prediction = load_model.predict([var])
     prob = load_model.predict_proba([var])
 
