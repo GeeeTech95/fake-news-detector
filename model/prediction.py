0225e28b27d551cbf447ac4abd6b61311907ebcc
kyle_coder <geeetech.inc@gmail.com>
2022-12-19 11:47:16 +0530

    ''

--- model/prediction.py
+++ model/prediction.py
@@ -6,7 +6,7 @@ import pickle
 #function to run for prediction
 def detect_fake_news(var):    
     #retrieving the best model for prediction call
-    load_model = pickle.load(open('model/final_model.sav', 'rb'))
+    load_model = pickle.load(open('final_model.sav', 'rb'))
     prediction = load_model.predict([var])
     prob = load_model.predict_proba([var])
 
